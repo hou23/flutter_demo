d757cb243f54998580e858cd0722c4f20944f614
yaojie.hou <yaojie.hou@change.so>
2019-07-15 18:37:43 +0800

    商城项目配置

---
+++ lib/config/service_url.dart
@@ -0,0 +1,4 @@
+const serviceUrl = 'https://test-java.change.so';
+const servicePath = {
+  'homePageContent': serviceUrl + 'wxmini/homePageContent', // 商城首页接口
+};
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,8 +1,52 @@
 import 'package:flutter/material.dart';
+import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  String homePageContent = '正在获取数据';
+
+  @override
+  void initState() {
+    getHomePageContent().then((val) {
+      setState(() {
+        homePageContent = val.toString();
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Text(homePageContent),
+      ),
+    );
+  }
+}
+
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+
+  SwiperDiy({Key key, this.swiperDataList}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333.0,
+      child: Swiper(
+        itemBuilder: (BuildContext context, int index) {
+          return Image.network("");
+        },
+        itemCount: null,
+      ),
+    );
   }
 }
